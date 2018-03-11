bd57e6e36a93e7eaf27c802b5fbb22eb3e44d05d
Unknown <dylan2malec@gmail.com>
2018-03-10 22:30:14 -0600

    stick it allllllllllllllllllllllllllll the way up there

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -18,7 +18,7 @@ class App extends Component {
 
     constructor(){
         super();
-        
+
         // Initialize Firebase
         firebase.initializeApp(config);
     }
@@ -28,16 +28,16 @@ class App extends Component {
         let firebaseRef = firebase.database().ref();
         let UsaskRef = firebaseRef.child("UniversityOfSaskatchewan");
         let UsaskCoursesRef = UsaskRef.child("courseList");
-        
+
         // TODO: This is for test purposes only, currently
         // set to CME 332
         let desiredCourseRef = UsaskCoursesRef.child("CME").child("332");
-        
+
         let _this = this;
-        
+
         desiredCourseRef.on("child_added", snap1 => {
             // console.log(snap1.key); // Debugging
-            
+
             _this.setState({
                 posts: _this.state.posts.concat({key : snap1.key, val : snap1.val()}),
                 loading: false
--- src/containers/SearchPage/index.js
+++ src/containers/SearchPage/index.js
@@ -17,12 +17,15 @@ import * as firebase from "firebase";
      super(props);
 
      this.state = {
-       selectedOption: 'class'
+       selectedOption: 'class',
+       sidebarOpen: false //Im a austin
      }
      this.selectClass = this.selectClass.bind(this);
      this.selectNote = this.selectNote.bind(this);
      this.selectTag = this.selectTag.bind(this);
      this.submitSearch = this.submitSearch.bind(this);
+     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);  //im a austin
+     this.onSetSidebarClose = this.onSetSidebarClose.bind(this);  //im a austin
    }
 
    selectClass() {
@@ -39,8 +42,59 @@ import * as firebase from "firebase";
      //TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO
    }
 
+   //im a austin
+   onSetSidebarOpen() {
+     this.setState({sidebarOpen: true});
+   }
+
+   //im a austin
+   onSetSidebarClose() {
+     this.setState({sidebarOpen: false});
+   }
+
+   //im a austin
+   SidebarOpenButton(props){
+     return(
+       <button onClick={props.onClick}>
+          ---
+       </button>
+     );
+   }
+
    render() {
+     //im a austin
+     var sidebarContent = <div id="sidebarStuff">
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Login/Register</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Search</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Classes</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Note Post</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Report</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Account</button>
+      <br />
+      <button className="sidebarButtons" onClick={this.onSetSidebarClose}>Close Sidebar</button>
+      <br />
+    </div>
+
      return (
+              //im a austin
+              /*<Sidebar sidebar={sidebarContent}
+                       open={this.state.sidebarOpen}
+                       onSetOpen={this.setSidebarOpen}>
+                <div id="sidebarOpen">
+                   <button id="sidebarOpen" onClick={this.onSetSidebarOpen}>Pages</button>
+                </div>
+                <div>
+                  <b id="home1">Welcome to the Ed-Kit App Homepage!</b>
+                  <br />
+                  <b id="home2">The sidebar on the left has all current the pages.</b>
+                </div>
+              </Sidebar>*/
+
          <div>
            <b id="txtTitle">Search Page</b>
            <br />
